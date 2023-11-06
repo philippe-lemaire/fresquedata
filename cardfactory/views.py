41b7ca978d6f96fd9337e81a4f87f27d9802e4b7
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-11-06 17:45:02 +0100

    now cards editable from front office

--- cardfactory/views.py
+++ cardfactory/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 from .models import Card
+from .forms import CardForm
 
 # Create your views here.
 
@@ -53,3 +56,17 @@ def card_list_per_batch(request, batch):
     batches = list(set([card.batch for card in all_cards]))
     context = {"card_list": queryset, "batches_list": batches}
     return render(request, template_name, context)
+
+
+@login_required
+def edit_card(request, pk):
+    card = Card.objects.get(pk=pk)
+    form = CardForm(request.POST or None, instance=card)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            url = reverse("cardfactory:card_detail", args=[card.pk])
+            return HttpResponseRedirect(url)
+    return render(
+        request, "cardfactory/edit_card.html", context={"form": form, "card": card}
+    )
